d08269d7dead2a9fc76c1d907e6cf0c5589da9c0
yassmhd <32752439+yassmhd@users.noreply.github.com>
2019-04-09 15:43:01 +0200

    fixed eslint

--- test/test.js
+++ test/test.js
@@ -12,11 +12,11 @@ var date = (new Date((new Date()).valueOf() + 3000)).toGMTString();
 
 var assert = require('assert');
 describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
     });
-  });
 });
 
 describe('simple-cookie',function(){
